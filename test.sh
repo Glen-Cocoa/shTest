d7a26f6ad104f3637721e80decfa06a3ffcdddef
Eli Steiger <eli.steiger@hyland.com>
2018-08-30 14:17:45 -0400

    test

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# My first script
+# git workflow 
 
 git pull
+echo "You have pulled from your upstream remote"
 git add *
+echo "Your repo's changes have been added"
 git commit
+echo "Your commit was formed successfully"
 git push
 
-echo "update successful"
